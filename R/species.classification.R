d4604fbc12f766f0a8ad3f61f4385b62b4575ba2
Juan Alberto Molina-Valero <52129566+Molina-Valero@users.noreply.github.com>
2024-05-24 13:50:35 +0200

    Last updates

--- R/species.classification.R
+++ R/species.classification.R
@@ -1,41 +1,59 @@
 
 
 
-species.classification <- function(data, tree.tls, dist = 0.1, median = NULL, type = 1){
+species.classification <- function(data, tree.tls, dist = 0.1, type = 3){
 
   data <- data[, c("x", "y", "z")]
 
   sp <- data.frame(tree = as.integer(), x = as.numeric(), y = as.numeric(), z = as.numeric())
 
+  if(type == 3){
+
+  voro <- sf::st_as_sf(data, coords = c("x", "y", "z"))
+  voronoi <- sf::st_as_sf(tree.tls, coords = c("x", "y"))
+  voronoi <- sf::st_collection_extract(sf::st_voronoi(do.call(c, sf::st_geometry(voronoi))))
+  voro$tree <- unlist(sf::st_intersects(voro, voronoi))
+
+  }
+
+
   for (i in tree.tls$tree) {
 
   tree <- tree.tls[tree.tls$tree == i, ]
 
+  if(type != 3){
   dat <- data[data$x < tree$x + tree$dbh / 100 &
               data$x > tree$x - tree$dbh / 100 &
               data$y < tree$y + tree$dbh / 100 &
               data$y > tree$y - tree$dbh / 100, ]
+  }
 
-  if(nrow(dat) < 1){
-    next}
 
-  if(type == 1){
-    dat <- dat[dat$z <= 1.65 & dat$z >= 0.95, ]}
+  if(nrow(dat) < 1){next}
 
-  if(type == 2){
-    dat <- dat[dat$z >= 0.95, ]}
+  if(type == 1){dat <- dat[dat$z <= 1.65 & dat$z >= 0.95, ]}
 
-  # dat$x <- dat$x - tree$x
-  # dat$y <- dat$y - tree$y
+  if(type == 2){dat <- dat[dat$z >= 0.95, ]}
 
-  # dat$phi_2<- atan2(dat$y, dat$x)
-  # dat$phi_2 <- ifelse(dat$phi_2 < 0, dat$phi_2 + (2 * pi), dat$phi_2)
+  if(type == 3){
 
-  # dat$col <- ifelse(dat$phi_2 < pi/2 & dat$phi_2 > 0, 1,
-  #                    ifelse(dat$phi_2 < pi & dat$phi_2 > pi/2, 2,
-  #                           ifelse(dat$phi_2 < pi*3/2 & dat$phi_2 > pi, 3, 4)))
+    dat <- as.data.frame(sf::st_coordinates(voro[voro$tree == i, ]))
+    if(nrow(dat) < 1){next}
 
-  # plot(data$x, data$y, asp = 1, col = data$col)
+    colnames(dat) <- c("x", "y", "z")
+
+
+    kk <- quantile(dat$z, probs = c(0.25,0.5,0.75))
+
+    dat <- dat[dat$z >= kk[1] - 1 - dist &
+               dat$z <= kk[1] + 1 + dist |
+               dat$z >= kk[2] - 1 - dist &
+               dat$z <= kk[2] + 1 + dist |
+               dat$z >= kk[3] - 1 - dist &
+               dat$z <= kk[3] + 1 + dist, ]
+    }
+
+  if(nrow(dat) < 1){next}
 
   dat$tree <- i
 
@@ -48,10 +66,19 @@ species.classification <- function(data, tree.tls, dist = 0.1, median = NULL, ty
   variables <- geometric.features(sp, dist)
 
   if(type == 1){
-  variables <- variables[dat$z < 1.6 & dat$z > 1, ]}
+  variables <- variables[variables$z < 1.6 & variables$z > 1, ]}
 
   if(type == 2){
-  variables <- variables[dat$z > 1, ]}
+  variables <- variables[variables$z > 1, ]}
+
+  if(type == 3){
+    variables <- variables[variables$z > kk[1] - 1 &
+                           variables$z < kk[1] + 1 |
+                           variables$z > kk[2] - 1 &
+                           variables$z < kk[2] + 1 |
+                           variables$z > kk[3] - 1 &
+                           variables$z < kk[3] + 1, ]
+    }
 
 
   variables <- merge(variables, sp[, c("point", "tree")], by = "point", all = FALSE)
@@ -78,27 +105,6 @@ species.classification <- function(data, tree.tls, dist = 0.1, median = NULL, ty
                                number_neighbors = tapply(variables$number_neighbors, variables$tree, mean, na.rm = TRUE),
                                surface_density = tapply(variables$surface_density, variables$tree, mean, na.rm = TRUE),
                                volume_density = tapply(variables$volume_density, variables$tree, mean, na.rm = TRUE))
-
-  if(!is.null(median)){
-  tree.variables <- data.frame(tree = tapply(variables$tree, variables$tree, median),
-                               first_eigenvalue = tapply(variables$first_eigenvalue, variables$tree, median, na.rm = TRUE),
-                               second_eigenvalue = tapply(variables$second_eigenvalue, variables$tree, median, na.rm = TRUE),
-                               third_eigenvalue = tapply(variables$third_eigenvalue, variables$tree, median, na.rm = TRUE),
-                               sum_eigenvalues = tapply(variables$sum_eigenvalues, variables$tree, median, na.rm = TRUE),
-                               omnivariance = tapply(variables$omnivariance, variables$tree, median, na.rm = TRUE),
-                               eigenentropy = tapply(variables$eigenentropy, variables$tree, median, na.rm = TRUE),
-                               PCA1 = tapply(variables$PCA1, variables$tree, median, na.rm = TRUE),
-                               PCA2 = tapply(variables$PCA2, variables$tree, median, na.rm = TRUE),
-                               anisotropy = tapply(variables$anisotropy, variables$tree, median, na.rm = TRUE),
-                               planarity = tapply(variables$planarity, variables$tree, median, na.rm = TRUE),
-                               linearity = tapply(variables$linearity, variables$tree, median, na.rm = TRUE),
-                               surface_variation = tapply(variables$surface_variation, variables$tree, median, na.rm = TRUE),
-                               sphericity = tapply(variables$sphericity, variables$tree, median, na.rm = TRUE),
-                               verticality = tapply(variables$verticality, variables$tree, median, na.rm = TRUE),
-                               number_neighbors = tapply(variables$number_neighbors, variables$tree, median, na.rm = TRUE),
-                               surface_density = tapply(variables$surface_density, variables$tree, median, na.rm = TRUE),
-                               volume_density = tapply(variables$volume_density, variables$tree, median, na.rm = TRUE))
-  }
 
   return(tree.variables)
 
